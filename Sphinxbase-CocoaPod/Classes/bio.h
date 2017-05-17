ab91031330be98b2a03d430518a727931662bb3d
Alexander Mentzen <mentzen@dig.de>
2017-05-17 16:28:43 +0200

    fix

--- Sphinxbase-CocoaPod/Classes/bio.h
+++ Sphinxbase-CocoaPod/Classes/bio.h
@@ -71,9 +71,9 @@
 #include <stdarg.h>
 
 /* Win32/WinCE DLL gunk */
-#include "sphinxbase_export.h>
-#include "prim_type.h>
-#include "byteorder.h>
+#include "sphinxbase_export.h"
+#include "prim_type.h"
+#include "byteorder.h"
 
 /** \file bio.h
  * \brief Cross platform binary IO to process files in sphinx3 format. 
